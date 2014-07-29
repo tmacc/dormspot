811c21be8c98e719baf9653746be718ef2ccbb4c
tmacc <tim.macchi@gmail.com>
2014-07-28 22:39:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dormspot::Application.config.secret_key_base = '3909a55502b10c438463b3bdbe1c82dc6fe597362aff2347cca2f5d174262778c331cf83bdf761da644a61d4a5a6476ae205e36a72a9928261bbae2e63d7e89d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dormspot::Application.config.session_store :cookie_store, key: '_dormspot_session'
