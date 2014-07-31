eaf290edf50fe83ccafd67c41e967d01b0677ff1
tmacc <tim.macchi@gmail.com>
2014-07-30 21:15:14 -0400

    further changes to devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Dormspot::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for heroku
+  #reminder to set to dormspot.com before launching
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
