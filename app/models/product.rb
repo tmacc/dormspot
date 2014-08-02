030a0d98ff83ef1832821c96fb44682b4cffba71
tmacc <tim.macchi@gmail.com>
2014-08-02 09:28:31 -0400

    added some styles to make pins bigger

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }
+	has_attached_file :image, :styles => { :medium => "600x600#", :thumb => "300x300#" }
 end
