b23374ef71dc06ae704893870aeee5bb7663ce8f
tmacc <tim.macchi@gmail.com>
2014-08-02 14:29:04 -0400

    added user name

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "600x600#", :thumb => "300x300#" }
+	validates :image, presence: true
+	validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	has_many :products
+
+  has_many :products, dependent: :destroy
+
+  validates :name, presence: true
 end
